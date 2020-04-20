339ac67782eb977419e5ce0cb7d701d5e729bf61
Bruce Schultz <bschultz013@gmail.com>
2020-04-20 11:06:48 +0200

    Add connect to __init__.py

--- src/ebel_rest/__init__.py
+++ src/ebel_rest/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for eBEL API client."""
 
-from .core import connect
+from ebel_rest.core import connect
 
 __author__ = """Christian Ebeling"""
 __email__ = 'Christian.Ebeling@scai.fraunhofer.de'
