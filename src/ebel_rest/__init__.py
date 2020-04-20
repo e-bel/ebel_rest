a0dee18a215d20b23d677d808b5713ce6f52a885
Bruce Schultz <bschultz013@gmail.com>
2020-04-20 10:58:44 +0200

    Remove unneccessary __init__.py

--- src/ebel_rest/__init__.py
+++ src/ebel_rest/__init__.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for eBEL API client."""
+
+from .core import connect
 
 __author__ = """Christian Ebeling"""
 __email__ = 'Christian.Ebeling@scai.fraunhofer.de'
