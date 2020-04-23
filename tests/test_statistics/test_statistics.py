d63e435943a820397beedd1603696924d1635cb1
Bruce Schultz <bschultz013@gmail.com>
2020-04-23 09:48:19 +0200

    Simplify tests

--- tests/test_statistics/test_statistics.py
+++ tests/test_statistics/test_statistics.py
@@ -4,36 +4,32 @@ from ..constants import USER, PASSWORD, DATABASE, SERVER
 
 
 class TestStatistics:
+    connect(USER, PASSWORD, SERVER, DATABASE)
 
     def test_summarize(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.summarize()
         assert stats.data is not None
         assert stats.table.shape == (5, 2)
 
     def test_publication_by_year(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.publication_by_year()
         assert stats.data is not None
         assert len(stats.table.columns) == 2
         assert len(stats.table.index) > 0
 
     def test_publication_by_number_of_statements(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.publication_by_number_of_statements()
         assert stats.data is not None
         assert len(stats.table.columns) == 6
         assert len(stats.table.index) > 0
 
     def test_last_author_by_number_of_publications(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.last_author_by_number_of_publications()
         assert stats.data is not None
         assert len(stats.table.columns) == 2
         assert len(stats.table.index) > 0
 
     def test_last_author_by_number_of_statements(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.last_author_by_number_of_statements()
         assert stats.data is not None
         assert len(stats.table.columns) == 2
@@ -47,33 +43,28 @@ class TestStatistics:
         assert len(stats.table.index) > 0
 
     def test_edges(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.edges()
         assert stats.data is not None
         assert len(stats.table.columns) == 2
         assert len(stats.table.index) > 0
 
     def test_nodes(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.nodes()
         assert stats.data is not None
         assert len(stats.table.columns) == 2
         assert len(stats.table.index) > 0
 
     def test_total_bel_nodes(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.total_bel_nodes()
         assert stats.data is not None
         assert stats.table.shape == (1, 1)
 
     def test_total_bel_edges(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.total_bel_edges()
         assert stats.data is not None
         assert stats.table.shape == (1, 1)
 
     def test_total_publications(self):
-        connect(USER, PASSWORD, SERVER, DATABASE)
         stats = statistics.total_publications()
         assert stats.data is not None
         assert stats.table.shape == (1, 1)
