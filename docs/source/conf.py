33e603a6adcbb21a33dbae0efd73dcff673c9023
Bruce Schultz <bschultz013@gmail.com>
2020-04-24 13:44:56 +0200

    Bump version: 1.0.7 → 1.0.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Christian Ebeling, Bruce Schultz'
 author = 'Christian Ebeling, Bruce Schultz'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.7'
+release = '1.0.8'
 
 
 # -- General configuration ---------------------------------------------------
