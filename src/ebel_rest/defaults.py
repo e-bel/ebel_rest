69bb26523db6f188afd5e749ccd89e89eb7afe9c
Bruce Schultz <bschultz013@gmail.com>
2020-04-27 15:13:59 +0200

    Add library path to defaults.py

--- src/ebel_rest/defaults.py
+++ src/ebel_rest/defaults.py
@@ -2,6 +2,7 @@ import os
 
 LIBRARY_NAME = 'ebel_rest'
 HOME = os.path.expanduser('~')
+LIBRARY_PATH = os.path.join(HOME, LIBRARY_NAME)
 
-pics_path = os.path.join(HOME, LIBRARY_NAME, 'pics/algorithms/')
+pics_path = os.path.join(LIBRARY_PATH, 'pics/algorithms/')
 os.makedirs(pics_path, exist_ok=True)
