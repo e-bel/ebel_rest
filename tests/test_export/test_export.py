fd79c4a48e162d682b592f1a3bf548478b026060
Bruce Schultz <bschultz013@gmail.com>
2020-04-28 09:37:53 +0200

    Update export tests to match warnings

--- tests/test_export/test_export.py
+++ tests/test_export/test_export.py
@@ -25,7 +25,7 @@ class TestExport:
         assert os.path.getsize(mfp) > 0
 
     def test_sif(self):
-        gfp, mfp = export_graph(graph_path=TEST_GFP, output_file_format='sif', mapping_path=TEST_MFP)
+        gfp, mfp = export_graph(graph_path=TEST_GFP, output_file_format='sif', mapping_path=TEST_MFP, graph_delim='\t')
         assert os.path.isfile(gfp)
         assert os.path.isfile(mfp)
         assert os.path.getsize(gfp) > 0  # Check file is not empty
@@ -48,10 +48,10 @@ class TestExport:
     def test_output_format_defense(self):
         with pytest.raises(ValueError) as e:
             gfp, mfp = export_graph(graph_path=TEST_GFP, output_file_format='foo', mapping_path=TEST_MFP)
-        assert str(e.value) == "output_file_format must be either 'lst', 'sif', or 'json'"
+        assert str(e.value) == "output_file_format must be either 'lst', 'sif', 'csv', or 'json'"
 
     def test_missing_map_path(self):
-        gfp, mfp = export_graph(graph_path=TEST_GFP, output_file_format='sif')
+        gfp, mfp = export_graph(graph_path=TEST_GFP, output_file_format='sif', graph_delim='\t')
 
     def test_delim_defense(self):
         with pytest.raises(ValueError) as e:
@@ -59,7 +59,7 @@ class TestExport:
                                     output_file_format='sif',
                                     mapping_path=TEST_MFP,
                                     graph_delim='^',)
-        assert str(e.value) == "delimiter must be either '\t', ',', ' '"
+        assert str(e.value) == "delimiter for a SIF must be either tab-separated ('\t') or space-separated (' ')"
 
     def test_remove_test_files(self):
         if os.path.isfile(TEST_GFP):
