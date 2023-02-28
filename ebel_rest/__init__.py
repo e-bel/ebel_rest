5728c14610707d9821cf136bdc50112c76cfed61
Bruce Schultz <bschultz013@gmail.com>
2023-02-28 14:00:13 +0100

    Bump version: 1.0.21 → 1.0.22

--- ebel_rest/__init__.py
+++ ebel_rest/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for eBEL API client."""
 __author__ = """Christian Ebeling"""
 __email__ = 'Christian.Ebeling@scai.fraunhofer.de'
-__version__ = '1.0.21'
+__version__ = '1.0.22'
 
