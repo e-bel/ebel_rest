eac01c883125f92bf3dcd7c04ca756a0015eb879
Bruce Schultz <bschultz013@gmail.com>
2023-02-28 14:03:22 +0100

    revert: restore root imports

--- ebel_rest/__init__.py
+++ ebel_rest/__init__.py
@@ -1,4 +1,10 @@
 """Top-level package for eBEL API client."""
+
+from ebel_rest.manager.core import connect
+from ebel_rest.manager.export import export_graph, Exporter
+from ebel_rest.manager import export, query, statistics
+
+
 __author__ = """Christian Ebeling"""
 __email__ = 'Christian.Ebeling@scai.fraunhofer.de'
 __version__ = '1.0.22'
